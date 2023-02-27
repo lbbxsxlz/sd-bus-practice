8f4bd0eefcd63cd039abc5508555ed5127a01b3f
BinbinLee <lbbxsxlz@gmail.com>
2023-02-27 15:25:47 +0800

    Update Calculator_Multiply.c

--- 0pointer/Calculator_Multiply.c
+++ 0pointer/Calculator_Multiply.c
@@ -3,30 +3,31 @@
 #include <systemd/sd-bus.h>
 
 int main(int argc, char *argv[]) {
-   sd_bus_error error = SD_BUS_ERROR_NULL;
-   sd_bus_message *m = NULL;
-   sd_bus *bus = NULL;
-   const char *path;
-   int answer;
-   int r, x, y;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+    sd_bus_message *m = NULL;
+    sd_bus *bus = NULL;
+    const char *path;
+    int answer;
+    int r, x, y;
 
-   if( argc <= 2 )
-   {
-        fprintf(stderr, "please input x y to multiply \n");
-        return -1;
-   }
-   x=atoi(argv[1]);
-   y=atoi(argv[2]);
+    if (argc <= 2 )
+    {
+          fprintf(stderr, "please input x y to multiply \n");
+          return -1;
+    }
+   
+    x=atoi(argv[1]);
+    y=atoi(argv[2]);
 
-   /* Connect to the system bus */
-   r = sd_bus_open_user(&bus);
-   if (r < 0) {
-      fprintf(stderr, "Failed to connect to user bus: %s\n", strerror(-r));
-      goto finish;
-   }
+    /* Connect to the session bus */
+    r = sd_bus_open_user(&bus);
+    if (r < 0) {
+        fprintf(stderr, "Failed to connect to user bus: %s\n", strerror(-r));
+        goto finish;
+    }
 
    /* Issue the method call and store the respons message in m */
-   r = sd_bus_call_method(bus,
+    r = sd_bus_call_method(bus,
                         "net.poettering.Calculator",           /* service to contact */
                         "/net/poettering/Calculator",          /* object path */
                         "net.poettering.Calculator",           /* interface name */
@@ -35,25 +36,25 @@ int main(int argc, char *argv[]) {
                         &m,                                    /* return message on success */
                         "xx",                                  /* input signature */
                         x,                                     /* first argument */
-                        y );                                   /* second argument */
-   if (r < 0) {
-      fprintf(stderr, "Failed to issue method call: %s\n", error.message);
-      goto finish;
-   }
+                        y);                                    /* second argument */
+    if (r < 0) {
+        fprintf(stderr, "Failed to issue method call: %s\n", error.message);
+        goto finish;
+    }
 
-   /* Parse the response message */
-   r = sd_bus_message_read(m, "x", &answer);
-   if (r < 0) {
-      fprintf(stderr, "Failed to parse response message: %s\n", strerror(-r));
-      goto finish;
-   }
+    /* Parse the response message */
+    r = sd_bus_message_read(m, "x", &answer);
+    if (r < 0) {
+        fprintf(stderr, "Failed to parse response message: %s\n", strerror(-r));
+        goto finish;
+    }
 
-   printf("Answer is: %d.\n", answer);
+    printf("Answer is: %d.\n", answer);
 
 finish:
-   sd_bus_error_free(&error);
-   sd_bus_message_unref(m);
-   sd_bus_unref(bus);
+    sd_bus_error_free(&error);
+    sd_bus_message_unref(m);
+    sd_bus_unref(bus);
 
-   return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+    return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
